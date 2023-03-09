be64b47bcb19120fc2a0cd076084eeab6051d2fb
Vishnu Pradeesh N <76156797+vishnupradeesh@users.noreply.github.com>
2023-03-09 22:20:30 +0530

    Create half_pyramid.c
    HALF PYRAMID IN C

---
+++ C-Program-Assignment-2/half_pyramid.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{	
+	int row,i,j;
+	printf("ENTER THE ROW SIZE : ");
+	scanf("%d",&row);
+	
+	for(i=0;i<=row;i++)
+	{
+		for(j=0;j<i;j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	
+	for(i=0;i<row;i++)
+	{
+		for(j=0;j<row;j++)
+		{
+			if(j >= (row-i-1))
+			{
+				printf("* ");
+			}
+			else{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
